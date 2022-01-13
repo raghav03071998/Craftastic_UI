3ee1db9ee8e7de19b6708a3a19372338c639985b
Raghav Agarwal <agarwalraghav0703@gmail.com>
2022-01-14 01:54:11 +0530

    SignUp page fix

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import SignUpService from '../services/SignUpService'
-
+import Footer from './Footer'
 function SignUp() {
 
     const userObj = {
@@ -27,29 +27,45 @@ function SignUp() {
     }
 
     return (
-    
-        <div id='signUpDiv'>
-
-            <form id='signUpForm' onSubmit={saveUser}>
-
-                <input id='name' type="text" placeholder='Your Name' name="name" onChange={onChange} required></input>
-
-                <input id='mobileNumber' type="text" placeholder='Mobile Number' name="mobileNumber"  onChange={onChange} required ></input>
-
-                <input id='email' type="text" placeholder='Email Id' name="email"  onChange={onChange} required ></input>
-
-                <input id='password' type="password" placeholder='Password' name="password"  onChange={onChange} required ></input>
-
-                <input id='button' type="submit" value="Submit" ></input>
-
-            </form>
-
+        <div id = "signUpDiv">
+            <img src="./assets/craftastic_logo.png" alt="Italian Trulli" id = "logo"></img>
+            <form id = "signUpForm">
+            <label id = "signUpText">Sign Up</label>
+            <input id='name' type="text" placeholder='Your Name' name="name" onChange={onChange} required></input>
+            <input id='mobileNumber' type="text" placeholder='Mobile Number' name="mobileNumber"  onChange={onChange} required ></input>
+            <input id='email' type="text" placeholder='Email Id' name="email"  onChange={onChange} required ></input>
+            <input id='password' type="password" placeholder='Password' name="password"  onChange={onChange} required ></input>
+            <input id='confirmPassword' type="password" placeholder='Confirm Password' name="password"  onChange={onChange} required ></input>
+            <input id = 'submit' type='submit' value= 'Create an account'></input>
             <p id='para'>Already have an account ?</p>
-
-            <a id='signin' href='/signin'>Sign In</a>
-
+            <input id = 'signIn' type='submit' value= 'Sign In'></input>
+            </form>
+           <Footer></Footer>
         </div>
-    )
+    
+        // <div id='signUpDiv'>
+
+        //     <form id='signUpForm' onSubmit={saveUser}>
+        //         <label id = "signUpText">Sign-Up</label>
+
+        //         <input id='name' type="text" placeholder='Your Name' name="name" onChange={onChange} required></input>
+
+        //         <input id='mobileNumber' type="text" placeholder='Mobile Number' name="mobileNumber"  onChange={onChange} required ></input>
+
+        //         <input id='email' type="text" placeholder='Email Id' name="email"  onChange={onChange} required ></input>
+
+        //         <input id='password' type="password" placeholder='Password' name="password"  onChange={onChange} required ></input>
+
+        //         <input id='button' type="submit" value="Submit" ></input>
+
+        //     </form>
+
+        //     <p id='para'>Already have an account ?</p>
+
+        //     <a id='signin' href='/signin'>Sign In</a>
+
+        // </div>
+    );
 }
 
 export default SignUp
